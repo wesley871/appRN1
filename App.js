2c44c39596b5948986b550c17941cd80cf5d7e7b
Wesley Araujo <wesleyaraujovieira@gmail.com>
2026-03-11 17:04:53 -0300

    testes sem alterar nada muito importante no codigo

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Pagina inicial do sistema!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+      <SafeAreaProvider>
+            <SafeAreaView style={styles.container}>
+                <Text>Pagina inicial</Text>
+                <Text>do sistema!</Text>
+                <StatusBar style="auto" />
+            </SafeAreaView>
+      </SafeAreaProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#faf",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
